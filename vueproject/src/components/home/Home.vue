f25d2b200e07b223bd624d5ca7d7c2ac495c10f2
scarlet-moon <978542146@qq.com>
2019-08-28 18:55:58 +0800

    导航未完成

---
+++ vueproject/src/components/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+
+  import Header  from '@/components/home/base/Header'
+  export default {
+    name: 'Home',
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style scoped >
+
+
+</style>
---
+++ vueproject/src/components/home/base/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">HTML</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3">CSS</el-menu-item>
+      <el-menu-item index="4">JS</el-menu-item>
+    </el-menu>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+      data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style scoped >
+
+
+</style>
